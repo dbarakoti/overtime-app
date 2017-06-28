6d431c31dc936c41b3dc8435a1c2860f199240cf
Dipesh Barakoti <dbarakoti@gmail.com>
2017-06-28 17:14:14 -0600

    Refactured index page to use Post partial

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,48 +2,47 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      login_as(user, :scope => :user)
-    end
+    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(@user, :scope => :user)
+  end
+
   describe 'index' do
     before do
       visit posts_path
     end
 
+    it 'can be reached successfully' do
+      expect(page.status_code).to eq(200)
+    end
 
-  	it 'can be reached successfully' do
-  		expect(page.status_code).to eq(200)
-  	end
-
-  	it 'has a title of Posts' do
-  		expect(page).to have_content(/Posts/)
-  	end
+    it 'has a title of Posts' do
+      expect(page).to have_content(/Posts/)
+    end
 
     it 'has a list of posts' do
-      post1 = Post.create(date: date.today, rationale: "Post1")
-      post2 = Post.create(date: date.today, rationale: "Post2")
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
       visit posts_path
-      expect(page).to have_content(/Posts1|Post2/)
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
-  	before do
-    
-  		visit new_post_path
-  	end
+    before do
+      visit new_post_path
+    end
 
-  	it 'has a new form that can be reached' do
-  		expect(page.status_code).to eq(200)
-  	end
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
 
-  	it 'can be created from new form page' do
+    it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
-  	end
+    end
 
     it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
