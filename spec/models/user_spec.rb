a26cf903946187028e60336ecab7148e0149af10
Dipesh Barakoti <dbarakoti@gmail.com>
2017-07-05 15:35:45 -0600

    Implemented edit and update functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+      @user = FactoryGirl.create(:user)
     end
+
    describe "creation" do
     it "can be created" do
       expect(@user).to be_valid
@@ -17,7 +18,7 @@ RSpec.describe User, type: :model do
   end
     describe "custom name methods" do
       it "has a full name method that combines first and last name" do
-        expect(@user.full_name).to eq("SNOW, JOHN")
+        expect(@user.full_name).to eq("SNOW, JON")
     end
   end
  end
