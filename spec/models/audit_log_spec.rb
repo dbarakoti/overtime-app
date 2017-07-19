38f05201914f15be9611dcce2a10857a523a95a8
Dipesh Barakoti <dbarakoti@gmail.com>
2017-07-19 14:32:01 -0600

    Implemented initial view code for audit logs

--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -28,9 +28,9 @@ RSpec.describe AuditLog, type: :model do
 	end
 
 	it 'it should have a start date equal to 6 days prior' do
-		new_audit_log = Auditlog.create(user_id: User.last.id)
-		expect(new_audit_log.start-date).to eq(Date.today - 6.days)
-	end
+		new_audit_log = AuditLog.create(user_id: User.last.id)
+		expect(new_audit_log.start_date).to eq(Date.today - 6.days)
+	  end
  end
 end
 
