906227c4f80b017de7442a6cbb5455c0eb22a1db
Dipesh Barakoti <dbarakoti@gmail.com>
2017-07-25 17:10:30 -0600

    Buit out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-@user = User.create!(email: "test@test.com", 
+@employee = Employee.create!(email: "test@test.com", 
 										 password: "asdfasdf",
 										 password_confirmation: "asdfasdf", 
 										 first_name: "Jon", 
 										 last_name: "Snow",
 										 phone: "8015124637")
 
-puts "1 User created"
+puts "1 employee created"
 
 AdminUser.create!(email: "admin@test.com",
 								 password: "asdfasdf",
@@ -14,17 +14,17 @@ AdminUser.create!(email: "admin@test.com",
 								 last_name: "Name",
 								 phone: "8015124637")
 
-puts "1 Admin User created"
+puts "1 Admin user created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 
 puts "3 Posts have been created"
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
